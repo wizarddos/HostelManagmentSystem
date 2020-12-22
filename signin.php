a1518c47d3ee01cc7c56296fa8c64281b7037a35
wizarddos <wizarddos@github.com>
2020-12-22 09:50:34 +0100

    upload index and login form

---
+++ signin.php
@@ -0,0 +1,12 @@
+<?php
+require_once "includes/fulllib-min.php";
+
+$login = $_POST['login'];
+$password = $_POST['pass'];
+
+if($returned = login($login,$password,"connect.php","users","user","pass")){
+    header("Location: adminpanel.php");
+}else{
+    echo $returned;
+    header("Location: index.html");
+}
